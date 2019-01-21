7ccf71feff1c85e7e849a31ad7f5fe53fb1ba55b
syedimranhassan <imran@HammadHussain-Laptop-128.arbisoft.com>
2019-01-21 21:43:08 +0500

    test celery configuration change

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -661,7 +661,7 @@ STATICFILES_DIRS = [
 ]
 
 # Locale/Internationalization
-TIME_ZONE = 'America/New_York'  # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+TIME_ZONE = 'Europe/London'  # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 LANGUAGE_CODE = 'en'  # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGES_BIDI = lms.envs.common.LANGUAGES_BIDI
 
